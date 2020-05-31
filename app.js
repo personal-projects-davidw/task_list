effeba1d32828593d5788f51cb61ba7b1a09cdb6
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-05-31 14:37:15 +0100

    Now added new task input animations for javascript click, focus & blur events.

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 
-// ********** CHECKBOXES **********
+function moveLabelUp() {
+    taskLabel.style.animationDuration = '0.1s';
+    taskLabel.style.animationTimingFunction = 'linear';
+    taskLabel.style.animationFillMode = 'forwards';
+    taskLabel.style.animationName = 'shift_up';
+    setTimeout(function(){
+        newTaskInput.style.outline = '2px solid var(--sec-color--)';
+        newTaskInput.style.borderBottom = 'none';
+        newTaskInput.focus();
+    }, 100); 
+}
+
+function moveLabelDown() {
+    taskLabel.style.animationDuration = '0.1s';
+    taskLabel.style.animationTimingFunction = 'linear';
+    taskLabel.style.animationFillMode = 'forwards';
+    taskLabel.style.animationName = 'shift_down';
+    setTimeout(function(){
+        newTaskInput.style.outline = 'none';
+        newTaskInput.style.borderBottom = '2px solid var(--sec-color--)';
+    }, 100); 
+}
+
+
+// ********** CHECK BOXES **********
 
 // get all current list tickbox elements
 let tickBoxes = document.getElementsByClassName('tick');
@@ -18,4 +42,20 @@ tickBoxes.forEach(element => {
                 break;
         }
     })
+});
+
+// ********** NEW TASK LABEL **********
+
+const taskLabel = document.querySelector('.new_input label');
+const newTaskInput = document.getElementById('new_task');
+console.log(newTaskInput);
+
+
+taskLabel.addEventListener('click', moveLabelUp);
+newTaskInput.addEventListener('click', e => {
+    moveLabelUp();
+});
+
+newTaskInput.addEventListener('blur', e => {
+    moveLabelDown();
 });
